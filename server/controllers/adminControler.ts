6d211d24929f964498089d0dc3e851be5ce67412
dan4inatora <jtolev@astea.net>
2021-02-15 02:08:36 +0200

    added some ts stuff

--- server/controllers/adminControler.ts
+++ server/controllers/adminControler.ts
@@ -1,25 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
 import { AdminService } from '../services/AdminService';
 
 const adminService = new AdminService();
 
 
-export const loginController = (req, res, next) => {
+export const loginController = (req : Request, res : Response, next : NextFunction) => {
   return adminService.login(req, res, next);
 };
 
-export const editUserController = async (req, res, next) => {
+export const editUserController = async (req : Request, res : Response, next : NextFunction) => {
   return adminService.editUser(req, res, next);
 };
 
-export const createUserController = async (req, res, next) =>{
+export const createUserController = async (req : Request, res : Response, next : NextFunction) =>{
   return adminService.createUser(req, res, next);
 }
 
-export const deleteUserController = (req, res, next) => {
+export const deleteUserController = (req : Request, res : Response, next : NextFunction) => {
   return adminService.deleteUser(req, res, next)
 };
 
-export const getAllUsersController = (req, res, next) => {
+export const getAllUsersController = (req : Request, res : Response, next : NextFunction) => {
   return adminService.getAllUsers(req, res, next)
 };
 
--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,21 +1,22 @@
+import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/UserService";
 
 const userService = new UserService();
 
-export const registerController = (req, res, next) => {
+export const registerController = (req : Request, res : Response, next : NextFunction) => {
   return userService.register(req, res, next);
 };
 
-export const loginController = async (req, res, next) => {
+export const loginController = async (req : Request, res : Response, next : NextFunction) => {
   return userService.login(req, res, next);
 };
 
 
-export const currentUserController = async (req, res, next) =>{
+export const currentUserController = async (req : Request, res : Response, next : NextFunction) =>{
   return userService.currentUser(req, res, next);
 }
 
-export const logoutController = (req, res, next) => {
+export const logoutController = (req : Request, res : Response, next : NextFunction) => {
   return userService.logout(req, res, next)
 };
 
