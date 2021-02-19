7be15951501c9c355d3309c628f72b47f1ddb257
dan4inatora <jtolev@astea.net>
2021-02-19 15:30:36 +0200

    Comments up and running

--- server/services/CommentService.ts
+++ server/services/CommentService.ts
@@ -4,6 +4,8 @@ import {Reacts} from '../constants/Reacts'
 import { CommentReacts } from "../models/CommentReacts";
 import { Not } from "typeorm/find-options/operator/Not";
 import { DeleteResult, UpdateResult } from "typeorm";
+import hotelService from "./HotelService";
+import { Hotel } from "../models/Hotel";
 
 
 class CommentService {
@@ -24,7 +26,7 @@ class CommentService {
 
     public async isUserReactedThumb(commentId: number, userId : number) : Promise<Boolean>{
 
-        const commentReact = await CommentReacts.findOne({where: {commentId, userId, thumbReact : Not(null)}}); 
+        const commentReact = await CommentReacts.findOne({where: {commentId, userId, thumbReact : 1}}); 
         if(commentReact != undefined){
             return true;
         }   
@@ -35,11 +37,14 @@ class CommentService {
 
     public async isUserReactedParty(commentId: number, userId : number) : Promise<Boolean>{
 
-        const commentReact = await CommentReacts.findOne({where: {commentId, userId,  partyReact : Not(null)}}); 
+        const commentReact = await CommentReacts.findOne({where: {commentId, userId,  partyReact : 1}}); 
+        console.log("What is your value", commentReact);
         if(commentReact != undefined){
+            console.log("Is it true", commentReact)
             return true;
         }   
         else{
+            console.log("Is it false", commentReact)
             return false;
         }
     }
@@ -60,21 +65,26 @@ class CommentService {
     public async deleteReact(commentId: number, userId : number, reactType : string) : Promise<DeleteResult>{
         let toBeDeleted : CommentReacts = null;
         if(reactType == Reacts.THUMB){
-            toBeDeleted = await CommentReacts.findOne({where:{commentId, userId, thumbReact: Not(null)}})
+            toBeDeleted = await CommentReacts.findOne({where:{commentId, userId, thumbReact: 1}})
         }
         else{
-            toBeDeleted = await CommentReacts.findOne({where:{commentId, userId, partyReact: Not(null)}})
+            toBeDeleted = await CommentReacts.findOne({where:{commentId, userId, partyReact: 1}})
         
         }
          
         return await CommentReacts.delete(toBeDeleted);
     }
 
-    public async addComment(hotelId : number, userId: number, comment: string) : Promise<Comment>{
+    public async addComment(userId: number, hotelId : number, hotelName: string, comment: string) : Promise<Comment>{
+        let hotel = await hotelService.getHotelById(hotelId);
+        if(hotel == undefined){ 
+            await hotelService.createHotel(hotelId, hotelName, 0)
+        }
         const newComment = new Comment();
         newComment.hotelId = hotelId;
         newComment.userId = userId;
         newComment.comment = comment;
+        console.log("COmment created", newComment);
         return await newComment.save();
     }
 
